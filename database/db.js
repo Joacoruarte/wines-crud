9d0b4b8425b645de2ac1ac6ef996edc1e8d94215
Joacoruarte <ruartejoaquin@gmail.com>
2022-04-26 10:28:22 -0300

    add config database

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 import {Sequelize} from 'sequelize';
-
-const db = new Sequelize('database_crud', 'root', 'SoyHenry', {
-    host: 'localhost',
+//mysql://@us-cdbr-east-05.cleardb.net/heroku_f6323bb80339f8b?reconnect=true
+const db = new Sequelize('heroku_f6323bb80339f8b', 'b82c79e6a90f7d', '690be15b', {
+    host: 'us-cdbr-east-05.cleardb.net',
     dialect: 'mysql'
 })
 
